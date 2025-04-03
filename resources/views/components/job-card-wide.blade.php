f626c5ac415c6ac68d8e16e5178cab0b50f87335
Micah <ismiki977@gmail.com>
2025-04-03 21:12:22 +0300

    first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,21 @@
+<div class=" flex gap-x-6 ">
+    <div>
+      <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+        <h3 class="font-bold text-xl group-hover:text-blue-800 transition-colors duration-500">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+
+    <div>
+        <x-tag>Vue</x-tag>
+        <x-tag>Backend</x-tag>
+        <x-tag>Frontend</x-tag>
+    </div>
+
+
+
+</div>
