0a424a684d5d2443b5aeb09374fbf0f348600ef3
Micah <ismiki977@gmail.com>
2025-04-05 01:15:19 +0300

    backend integration

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,17 +1,18 @@
+@props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracast</div>
+    <div class="self-start text-sm">{{$job->employer->name}}</div>
     <div class="py-8 ">
-        <h3 class="group-hover:text-blue-800 font-bold text-xl transition-colors duration-500">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-800 font-bold text-xl transition-colors duration-500">{{$job->title}}</h3>
+        <p class="text-sm mt-4">{{$job->schedule}} - {{$job->salary}}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-           
-            <x-tag size="small">Manager</x-tag>
-            <x-tag  size="small">Backend</x-tag>
-            <x-tag  size="small">Frontend</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :tag="$tag" size="small" />
+            @endforeach
+
         </div>
 
-      <x-employer-logo :width="42"/>
+        <x-employer-logo :width="42" />
     </div>
 </x-panel>
